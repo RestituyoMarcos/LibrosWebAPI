6c165f93445f555a0057862dfa4ce17399a53bbf
RestituyoMarcos <restituyo.rosario.marcos@gmail.com>
2025-04-29 01:31:30 -0400

    Libros Completados

---
+++ LibrosWebAPI/Controllers/BookController.cs
@@ -0,0 +1,81 @@
+﻿using BooksWebAPI.Commands;
+using BooksWebAPI.Models;
+using BooksWebAPI.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace LibrosWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BookController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            return Ok(await _mediator.Send(new GetAllBooks()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _mediator.Send(new GetBookById() { Id = id });
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> CreateBook(Book book)
+        {
+            var createdBook = await _mediator.Send(new CreateBook(book));
+            return CreatedAtAction(nameof(GetBook), new { id = createdBook.Id }, createdBook);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest("El Id  con coincide con el del libro seleccionado");
+            }
+
+            try
+            {
+                await _mediator.Send(new UpdateBook(id, book));
+            }
+            catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            try
+            {
+                await _mediator.Send(new DeleteBook(id));
+            }
+            catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
--- LibrosWebAPI/Program.cs
+++ LibrosWebAPI/Program.cs
@@ -1,11 +1,31 @@
+using BooksWebAPI.Services;
+using MediatR;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddHttpClient<IBookService, BookService>();
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var apiBaseUrl = builder.Configuration.GetSection("ExternalAPI:BaseUrl").Value;
+var webAppURL = builder.Configuration.GetSection("ExternalAPI:WebAppURL").Value;
+
+builder.Services.AddHttpClient("BaseURL", client =>
+{
+    client.BaseAddress = new Uri(apiBaseUrl);
+});
+
+builder.Services.AddCors(op =>
+{
+    op.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins(webAppURL).AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
@@ -21,5 +41,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors();
 
 app.Run();
